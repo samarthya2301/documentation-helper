b40654b6b9cd7dadebaa1e955f9143b42726c52d
samarthya2301 <samarthya2301@gmail.com>
2025-09-09 17:53:03 +0530

    ingestion pipeline complated

--- upload_scripts/upload_to_pinecone.py
+++ upload_scripts/upload_to_pinecone.py
@@ -4,6 +4,9 @@ from typing import List, Union
 from dotenv import load_dotenv
 from langchain_community.document_loaders import (NotebookLoader, TextLoader,
                                                   UnstructuredMarkdownLoader)
+from langchain_openai import OpenAIEmbeddings
+from langchain_pinecone import PineconeVectorStore
+from langchain_text_splitters import RecursiveCharacterTextSplitter
 
 load_dotenv()
 
@@ -21,7 +24,6 @@ def get_loader_based_on_extension(file_path: str) -> Union[None, NotebookLoader,
 	"""
 	Return a loader for the corresponding file type.
 	"""
-	print(file_path)
 	extension = file_path.split(".")[-1]
 	extension_to_loader_dict = LOADERS_FOR_EXTENSIONS
 	loader = extension_to_loader_dict.get(extension)
@@ -34,7 +36,7 @@ def load_documentation_files(folder_path="./langchain-docs") -> List:
 	Extensions are used to get the corresponding loaders.
 	"""
 	documents = []
-	print("\n\nLoading the following files ...")
+	print("\n\nLoading Documents ...")
 
 	for dirpath, dirnames, filenames in os.walk(folder_path):
 
@@ -46,12 +48,35 @@ def load_documentation_files(folder_path="./langchain-docs") -> List:
 			if loader:
 				documents.extend(loader.load())
 
-	print("\nTotal Files Loaded: ", len(documents))
+	print("Total Documents Loaded: ", len(documents))
 	return documents
+
+
+def create_document_chunks(splitter: RecursiveCharacterTextSplitter, documents: List) -> List:
+	"""
+	Convert documents into required chunks, with RecursiveCharacterTextSplitter.
+	"""
+	print(f"\n\nChunk Size={splitter._chunk_size}, Overlap={splitter._chunk_overlap}, Creating Chunks ...")
+	chunks = splitter.split_documents(documents=documents)
+	print("Total Chunks Created: ", len(chunks))
+	return chunks
+
+
+def push_data_to_pinecone_vectorstore(embedding: OpenAIEmbeddings, chunks: List):
+	"""
+	Pushes the data provided to the Vector Store
+	"""
+	print(f"\n\nPushing data to Vector Store ...")
+	PineconeVectorStore.from_documents(embedding=embedding, documents=chunks, index_name=os.environ["PINECONE_INDEX_NAME"])
+	print("Pushed Data to Vector Store\n\n")
 
 
 if __name__ == "__main__":
 
 	documents = load_documentation_files()
 
-	# get split documents
+	text_splitter = RecursiveCharacterTextSplitter(chunk_size=1000, chunk_overlap=100)
+	chunked_documents = create_document_chunks(splitter=text_splitter, documents=documents)
+
+	openai_embeddings = OpenAIEmbeddings()
+	push_data_to_pinecone_vectorstore(openai_embeddings, chunked_documents)
